aee2b242cccd1ab8b2ee9219b566796fad64b461
SergioGonzalez24 <sergiomanuel2020@gmail.com>
2021-06-03 19:15:58 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// Proyecto Integrador Programacion orientada a objetos
+
+/* 
+Codigo desaroyado por:
+Sergio Gonzalez - A01745446
+Jorge Rojas - A01745
+
+Fecha de creacion 03/05/2021
+
+Modelar un servicio de Streaming Utilizando OPP en c++
+*/
+
+int main(int argc, char) {
+    return 0;
+}
