0af8578e06ee9ea89c960e2794e00e0e3b004d76
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-06-09 17:14:34 -0500

    corregido las clases

--- src/ServicioStreaming.cpp
+++ src/ServicioStreaming.cpp
@@ -9,12 +9,8 @@ ServicioStreaming::ServicioStreaming() {
 
 ServicioStreaming::ServicioStreaming(string nServicio, string nUsr, string idUsr) {
     this-> nombre = nServicio;
-    //ATORADO
-    Usuario usr1(nUsr,idUsr);
-    usr1.setNombreUsuario(nUsr);
-    usr1.setIdUsuario(idUsr);
-
-
-
-
+    datosUsuario = Usuario(nUsr, idUsr);
 }
+
+
+
