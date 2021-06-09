6060e64604d0547064f1a8f54fc36f1d3b2245b2
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-06-08 23:32:19 -0500

    .

--- src/Serie.cpp
+++ src/Serie.cpp
@@ -1,6 +1,6 @@
 //Realizado por Jorge Rojas, A01745334
 
-#include "Serie.hpp"
+#include "../include/Serie.hpp"
 #include <iostream>
 
 //Constructores
@@ -10,3 +10,4 @@ Serie::Serie(string t, int d, string n, string g){
     this-> nombre = n;
     this-> genero = g;
 }
+
